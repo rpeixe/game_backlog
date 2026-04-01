a3976052b433283fb086ec3c50cf80314e03d4f8
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2026-04-01 16:27:45 -0300

    Added associations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,5 @@
 class Game < ApplicationRecord
+  has_many :backlog_entries, dependent: :destroy
+  has_many :users, through: :backlog_entries
+  has_many :reviews, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
+  has_many :backlog_entries, dependent: :destroy
+  has_many :games, through: :backlog_entries
+  has_many :reviews, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
